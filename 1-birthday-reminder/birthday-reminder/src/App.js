cbc123a0d40b49981413afd479a6aa5f732c782e
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-20 15:34:30 +0200

    (main/birthdayreminder): create List component

--- 1-birthday-reminder/birthday-reminder/src/App.js
+++ 1-birthday-reminder/birthday-reminder/src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import './App.css'
 import data from './data'
+import List from './List'
 
 function App() {
   return <h2>Reminder project setup</h2>
+  
 }
 
 export default App;
