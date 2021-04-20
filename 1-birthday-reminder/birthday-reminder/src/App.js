920d97a58b60929aadaa980bbe47c804f4b5daeb
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-20 15:39:41 +0200

    (main/birthdayreminder): component implementation and onClick envent on button

--- 1-birthday-reminder/birthday-reminder/src/App.js
+++ 1-birthday-reminder/birthday-reminder/src/App.js
@@ -3,7 +3,13 @@ import data from './data'
 import List from './List'
 
 function App() {
-  return <h2>Reminder project setup</h2>
+  return <main>
+    <section className="container">
+      <h3>0 Anniversaire(s) aujourd'hui</h3>
+      <List/>
+      <button onClick={()=> console.log('click sur le bouton')}>Valider</button>
+    </section>
+  </main>
   
 }
 
