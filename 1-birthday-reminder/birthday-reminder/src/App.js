1d9e42d950e5692a6ae95c8b33f86250bc6793c1
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-20 15:52:44 +0200

    (main/birthdayreminder): definig state and passing props to List component

--- 1-birthday-reminder/birthday-reminder/src/App.js
+++ 1-birthday-reminder/birthday-reminder/src/App.js
@@ -1,12 +1,20 @@
-import './App.css'
+// Functional import
+import React, { useState } from 'react'
+// Data import
 import data from './data'
+// Component import
 import List from './List'
+// Esthetical import
+import './App.css'
 
 function App() {
+
+  // Definig state with people data file
+  const [people, setPeople] = useState(data)
   return <main>
     <section className="container">
-      <h3>0 Anniversaire(s) aujourd'hui</h3>
-      <List/>
+      <h3>{people.length} Anniversaire(s) aujourd'hui</h3>
+      <List people={people}/>
       <button onClick={()=> console.log('click sur le bouton')}>Valider</button>
     </section>
   </main>
