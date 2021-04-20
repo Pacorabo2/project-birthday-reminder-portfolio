07b9ea58033b28afd5be0057649cb715e3a999a1
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-20 21:50:42 +0200

    (main/birthday-reminder): change structure site

--- 1-birthday-reminder/birthday-reminder/src/App.js
+++ 1-birthday-reminder/birthday-reminder/src/App.js
@@ -1,24 +1,24 @@
 // Functional import
 import React, { useState } from 'react'
-// Data import
 import data from './data'
-// Component import
-import List from './List'
-// Esthetical import
-import './App.css'
+// Components import
+import List from './components/List'
 
 function App() {
 
-  // Definig state with people data file
+  // Define the state with the data
   const [people, setPeople] = useState(data)
-  return <main>
-    <section className="container">
-      <h3>{people.length} Anniversaire(s) aujourd'hui</h3>
-      <List people={people}/>
-      <button onClick={()=> setPeople([])}>Valider</button>
-    </section>
-  </main>
   
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.lenght} birthday today</h3>
+        <List people={people}/>
+        <button onClick={() => setPeople([])}>Vider la liste</button>
+      </section>
+    </main>
+    
+  );
 }
 
 export default App;
