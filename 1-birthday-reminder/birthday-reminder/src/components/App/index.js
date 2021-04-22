d9979149230fe9d8c63f0d8799c0173be23d93e7
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-22 13:59:25 +0200

    (main/Header): Header component creation & css and import in App

---
+++ 1-birthday-reminder/birthday-reminder/src/components/App/index.js
@@ -0,0 +1,32 @@
+// Functional import
+import React, { useState } from 'react'
+import data from '../../data'
+// Components import
+import List from '../List'
+import Button from '../Button'
+import Header from '../Header'
+//Esthetical import
+import Cake from '../../../src/assets/cake.png'
+
+
+function App() {
+
+  // Define the state with the data
+  const [people, setPeople] = useState(data)
+  
+  return (
+    <main>
+      <section className="container">
+        <Header/>
+        {/* <h3>{people.lenght} birthday today</h3> */}
+        {/* <List people={people}/> */}
+        <Button/>
+        {/* <button onClick={() => setPeople([])}>Vider la liste</button> */}
+        {/* <img src={Cake} alt=""/> */}
+      </section>
+    </main>
+    
+  );
+}
+
+export default App;
---
+++ 1-birthday-reminder/birthday-reminder/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './style.css'
+
+const Header = () => {
+  return (
+    <div className="head-cont">
+      Header
+    </div>
+  )
+}
+
+export default Header
