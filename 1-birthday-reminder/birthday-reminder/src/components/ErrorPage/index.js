e119db51586893a51cf226a2ad2c1a857261758c
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-22 19:46:41 +0200

    (main/ErrorPage): ErrorPage component modifications

---
+++ 1-birthday-reminder/birthday-reminder/src/components/ErrorPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './style.css'
+
+const ErrorPage = () => {
+  return (
+    <div className="container">
+      <p>Oups, une erreur est survenue !</p>
+    </div>
+  )
+}
+
+export default ErrorPage
