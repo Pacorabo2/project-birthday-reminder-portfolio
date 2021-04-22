329715f5e61ae1a19e5e70125757be599f2dc7fa
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-22 10:12:17 +0200

    (main/Firebase): firebase init and config

---
+++ 1-birthday-reminder/birthday-reminder/src/components/Firebase/firebase.js
@@ -0,0 +1,19 @@
+import app from 'firebase/app'
+
+var config = {
+  apiKey: "AIzaSyB0Ve-ntaidOXZfK7eP5WpD5OUqM_x8FMo",
+  authDomain: "birthday-reminder-fdca4.firebaseapp.com",
+  projectId: "birthday-reminder-fdca4",
+  storageBucket: "birthday-reminder-fdca4.appspot.com",
+  messagingSenderId: "340934199877",
+  appId: "1:340934199877:web:3c8d6b0464a0dcc934712c"
+};
+
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config)
+  }
+}
+
+export default firebase
