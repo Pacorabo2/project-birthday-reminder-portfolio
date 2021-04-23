78a735b723ff24cf9430ff63e4c14e1e58e2c3e5
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-23 12:07:21 +0200

    (main/Login&Signup): Form implementation and fix inversion between login and signup

--- 1-birthday-reminder/birthday-reminder/src/components/Login/index.js
+++ 1-birthday-reminder/birthday-reminder/src/components/Login/index.js
@@ -1,13 +1,34 @@
 import React from 'react'
+import Button from '../Button'
 
 import './style.css'
 
 const Login = () => {
   return (
-    <div>
-      Login
+    <div className="container">
+      <h2>Connexion</h2>
+      <form>
+        <div className="formContent">
+          <div className="inputBox">
+            <input type="email" autoComplete="off" required/>
+            <label htmlFor="email">Email</label>
+          </div>
+          <div className="inputBox">
+            <input type="text" autoComplete="off" required/>
+            <label htmlFor="password">Mot de passe</label>
+          </div>
+          <Button/>
+        </div>
+      </form>
+      <div className="linkContainer">
+        <p className="simpleLink"><a href="/login">Nouveau sur Birthday App ? Inscrivez-vous maintenant.</a></p>
+        <p className="simpleLink"><a href="/forgetpassword">Mot de passe oublié ?</a></p>
+      </div>
     </div>
+    
   )
 }
 
 export default Login
+
+
--- 1-birthday-reminder/birthday-reminder/src/components/Signup/index.js
+++ 1-birthday-reminder/birthday-reminder/src/components/Signup/index.js
@@ -6,24 +6,31 @@ import './style.css'
 const Signup = () => {
   return (
     <div className="container">
+      <h2>Connexion</h2>
       <form>
         <div className="formContent">
-          <h2>Connexion</h2>
           <div className="inputBox">
-            <input type="text"/>
+            <input type="text" id="pseudo" autoComplete="off" required />
+            <label htmlFor="pseudo">Pseudo</label>
+          </div>
+          <div className="inputBox">
+            <input type="email" id="email" autoComplete="off" required />
             <label htmlFor="email">Email</label>
           </div>
           <div className="inputBox">
-            <input type="text"/>
-            <label htmlFor="password">Mot de passe</label>
+            <input type="password" id="password" autoComplete="off" required />
+            <label htmlFor="password">Mot de Passe</label>
+          </div>
+          <div className="inputBox">
+            <input type="password" id="confirmPassword" autoComplete="off" required />
+            <label htmlFor="password">Confirmez le mot de Passe</label>
           </div>
           <Button/>
-          <div className="linkContainer">
-            <p className="simpleLink"><a href="/login">Nouveau sur Birthday App ? Inscrivez-vous maintenant.</a></p>
-            <p className="simpleLink"><a href="/forgetpassword">Mot de passe oublié? Récupérez-le ici.</a></p>
-          </div>
         </div>
       </form>
+      <div className="linkContainer">
+        <p className="simpleLink"><a href="/login">Déjà inscrit sur Birthday App ? Connectez-vous.</a></p>
+      </div>
     </div>
   )
 }
