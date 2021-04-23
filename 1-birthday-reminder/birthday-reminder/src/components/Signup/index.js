39fab138fc2a3f9074ab7ea0d06814ddea1b8006
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-23 14:16:06 +0200

    (main/Signup): state on component and get users information on state

--- 1-birthday-reminder/birthday-reminder/src/components/Signup/index.js
+++ 1-birthday-reminder/birthday-reminder/src/components/Signup/index.js
@@ -1,28 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../Button'
 
 import './style.css'
 
 const Signup = () => {
+
+  const data = {
+    pseudo: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  }
+
+  const [loginData, setLoginData] = useState(data)
+
+  const handleChange = (e) => {
+    setLoginData({...loginData, [e.target.id]: e.target.value})
+  }
+
+  const { pseudo, email, password, confirmPassword } = loginData
+
   return (
     <div className="container">
       <h2>Inscription</h2>
       <form>
         <div className="formContent">
           <div className="inputBox">
-            <input type="text" id="pseudo" autoComplete="off" required />
+            <input onChange={handleChange} value={pseudo} type="text" id="pseudo" autoComplete="off" required />
             <label htmlFor="pseudo">Pseudo</label>
           </div>
           <div className="inputBox">
-            <input type="email" id="email" autoComplete="off" required />
+            <input onChange={handleChange} value={email} type="email" id="email" autoComplete="off" required />
             <label htmlFor="email">Email</label>
           </div>
           <div className="inputBox">
-            <input type="password" id="password" autoComplete="off" required />
+            <input onChange={handleChange} value={password} type="password" id="password" autoComplete="off" required />
             <label htmlFor="password">Mot de Passe</label>
           </div>
           <div className="inputBox">
-            <input type="password" id="confirmPassword" autoComplete="off" required />
+            <input onChange={handleChange} value={confirmPassword} type="password" id="confirmPassword" autoComplete="off" required />
             <label htmlFor="password">Confirmez le mot de Passe</label>
           </div>
           <Button/>
