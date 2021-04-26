e6237dbd9e2b4df1e97a61c472e12701ec3ecf31
Nicolas Plaza <pacorabo2@hotmail.com>
2021-04-26 10:05:53 +0200

    (main/Login): state, onchange event definition on input to push the values on state

--- 1-birthday-reminder/birthday-reminder/src/components/Login/index.js
+++ 1-birthday-reminder/birthday-reminder/src/components/Login/index.js
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../Button'
 import { Link } from 'react-router-dom'
 
 import './style.css'
 
 const Login = () => {
+
+  // Data definition
+  const data = {
+    email: "",
+    password: "",
+  }
+
+  const [loginData, setLoginData] = useState(data)
+
+  const handleChange = (e) => {
+    setLoginData({...loginData, [e.target.id]: e.target.value})
+    // console.log(e.target.value);
+  }
+
+  const { email, password } = loginData
+
   return (
     <div className="container">
       <h2>Connexion</h2>
       <form>
         <div className="formContent">
           <div className="inputBox">
-            <input type="email" autoComplete="off" required/>
+            <input onChange={handleChange} type="email" id="email" value={email} autoComplete="off" required/>
             <label htmlFor="email">Email</label>
           </div>
           <div className="inputBox">
-            <input type="text" autoComplete="off" required/>
+            <input onChange={handleChange} type="text" id="password" value={password} autoComplete="off" required/>
             <label htmlFor="password">Mot de passe</label>
           </div>
           <Button/>
