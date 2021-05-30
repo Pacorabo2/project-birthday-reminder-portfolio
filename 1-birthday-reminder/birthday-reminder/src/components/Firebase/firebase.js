318f5fa62a2a28689112b55115e012bb92700fa4
Nicolas Plaza <pacorabo2@hotmail.com>
2021-05-30 10:48:55 +0200

    (brthdayReminder/firebaseBug): changing dotenv

--- 1-birthday-reminder/birthday-reminder/src/components/Firebase/firebase.js
+++ 1-birthday-reminder/birthday-reminder/src/components/Firebase/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/firestore'
 import 'firebase/storage'
 
 const config = {
-  apiKey: "AIzaSyB0Ve-ntaidOXZfK7eP5WpD5OUqM_x8FMo",
-  authDomain: "birthday-reminder-fdca4.firebaseapp.com",
-  projectId: "birthday-reminder-fdca4",
-  storageBucket: "birthday-reminder-fdca4.appspot.com",
-  messagingSenderId: "340934199877",
-  appId: "1:340934199877:web:3c8d6b0464a0dcc934712c"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 }
 
 class Firebase {
